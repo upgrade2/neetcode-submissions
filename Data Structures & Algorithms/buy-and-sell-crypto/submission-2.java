8827452d2f01b0b7fe1da87d10bcb6c2a98382d0
Ajay kumar <66551481+upgrade2@users.noreply.github.com>
2026-08-14 00:30:28 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice =Integer.MAX_VALUE;
+        int maxProfit=0;
+        for(int price:prices){
+            minPrice = Math.min(price,minPrice);
+            maxProfit = Math.max(price-minPrice,maxProfit);
+        }
+        return maxProfit;
+    }
+}
